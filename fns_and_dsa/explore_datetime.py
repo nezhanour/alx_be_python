b0745d9a3c245e969178b5223dbf271be97bfcf5
nezhanour <nezha95nour@gmail.com>
2024-09-29 21:12:57 +0100

    fixed formatted date explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,5 +1,3 @@
-
-from datetime import date
 from datetime import datetime
 from datetime import timedelta
 
@@ -14,8 +12,9 @@ print("Current date and time: ",display_current_datetime())
 
 number_of_days = int(input("enter a number of days (as an integer): "))
 def calculate_future_date():
-    future_date = date.today()
-    return future_date + timedelta(number_of_days)
+    future_date = datetime.today()
+    future_date += timedelta(number_of_days)
+    return future_date.strftime("%Y-%m-%d")
 
 print("Future date: ", calculate_future_date())
 
