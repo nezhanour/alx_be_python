f6cc26a517900bcabefc29b732c733e5eecc2b1c
nezhanour <nezha95nour@gmail.com>
2024-10-06 17:12:34 +0100

    re-Fixed float in display_balance method

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,4 +15,4 @@ class BankAccount:
     
     
     def display_balance(self):
-        print(f"Current Balance: ${float(self.account_balance)}") 
+        print(f"Current Balance: ${self.account_balance:.2f}") 
