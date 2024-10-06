8773ac1d921a16fd492f64564fad434adea0b928
nezhanour <nezha95nour@gmail.com>
2024-10-06 17:57:01 +0100

    Added '.' in 'Error: Cannot divide by zero.' return statment

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,7 +3,7 @@ def safe_divide(numerator, denominator):
     try:
         float(numerator) / float(denominator)
     except ZeroDivisionError:
-        return "Error: Cannot divide by zero"
+        return "Error: Cannot divide by zero."
     except ValueError:
         return "Error: Please enter numeric values only."
     else:
