9c8ce08cea1d0234f600c6544eba69d325f42714
nezhanour <nezha95nour@gmail.com>
2024-10-12 21:27:51 +0100

    Added library_system.py

---
+++ oop/library_system.py
@@ -0,0 +1,25 @@
+class Book:
+    def __init__(self, title: str, author: str):
+        self.title = title
+        self.author = author
+
+
+class EBook(Book):
+    def __init__(self, title, author, file_size: int):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count: int):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+class Library:
+    books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        pass
+
