52802254b5bfc1f504a05ff56e4d9c4583c1d506
nezhanour <nezha95nour@gmail.com>
2024-10-12 21:38:31 +0100

    Added '__str__' to methods in library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,16 +3,25 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
 
 class EBook(Book):
     def __init__(self, title, author, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File size: {self.file_size}KB"
+
 class PrintBook(Book):
     def __init__(self, title, author, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     books = []
@@ -21,5 +30,5 @@ class Library:
         self.books.append(book)
 
     def list_books(self):
-        pass
+        print(self.books)
 
